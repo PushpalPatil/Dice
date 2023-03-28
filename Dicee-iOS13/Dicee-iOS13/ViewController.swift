ed31c259a3529af3d276fba272861773b2ceeaaa
Pushpal Patil <gauripushp@gmail.com>
2023-03-28 16:02:33 -0700

    Add files via upload
    dice game finished

---
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    //var leftDiceNumber = 1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        // WHO.WHAT = VALUE
+        //diceImageView1.alpha = 0.5
+        
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        // set as 'let' not var because let allows it to be a static variable
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+        // diceArray[Int.random(in: 0...5)]
+        
+        
+    }
+    
+}
+
